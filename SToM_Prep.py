d57370ab262a5a2a2273bdd2d7e2fd0566648de9
Sophie <sophiedarrouxmartin@gmail.com>
2018-11-09 11:02:49 +0000

    update stom_prep 09/11

--- SToM_Prep.py
+++ SToM_Prep.py
@@ -15,49 +15,60 @@ data = tools.generate_data()
 
 # Generate histogram data
 bin_heights, bin_edges = np.histogram(data, bins=30, range=(104,155))
-bincenters = 0.5*(bin_edges[1:]+bin_edges[:-1])
+bin_centers = 0.5*(bin_edges[1:]+bin_edges[:-1])
 mean_std = np.sqrt(bin_heights) # Using Poisson error sqrt for the height
 widths = bin_edges[1:]-bin_edges[:-1]
 
 # Plotting data with 30 bins
 fig, (ax1, ax2) = plt.subplots(2,1, figsize=(8,15), sharex = True)
-ax1.bar(bincenters, bin_heights, width=width, color='orange', yerr=mean_std, error_kw=dict(elinewidth=0.5,ecolor='black'))
+ax1.bar(bin_centers, bin_heights, width=widths, color='orange', 
+        yerr=mean_std, error_kw=dict(elinewidth=0.5,ecolor='black'))
 ax1.set_title('Bins = 30')
 ax1.set_ylabel('Number of entries')
 ax1.set_xlabel('Mass (GeV)')
 
 # Plotting with 300 bins
 bin_heights2, bin_edges2 = np.histogram(data, bins=300, range=(104,155))
-bincenters2 = 0.5*(bin_edges2[1:]+bin_edges2[:-1])
+bin_centers2 = 0.5*(bin_edges2[1:]+bin_edges2[:-1])
 mean_std2 = np.sqrt(bin_heights2) # Using Poisson error sqrt for the height
 widths2 = bin_edges2[1:]-bin_edges2[:-1]
 
-ax2.bar(bincenters2, bin_heights2, width=width2, color='blue', yerr=mean_std2, error_kw=dict(elinewidth=0.5,ecolor='black'))
+ax2.bar(bin_centers2, bin_heights2, width=widths2, color='blue', 
+        yerr=mean_std2, error_kw=dict(elinewidth=0.5,ecolor='black'))
 ax2.set_title('Bins = 300')
 ax2.set_ylabel('Number of entries')
 ax2.set_xlabel('Mass (GeV)')
 plt.show()
 
-plt.figure(1)
+plt.figure()
 plt.subplot(2,1,1)
-plt.errorbar(bincenters, bin_heights, yerr=mean_std, xerr=width,
+plt.errorbar(bin_centers, bin_heights, yerr=mean_std, xerr=widths,
              fmt='.', ecolor='black', elinewidth=0.5, capsize=1)
-plt.title('Scatter plot of binned data')
 plt.ylabel('Number of entries')
 plt.xlabel('Mass (Gev)')
 
 # Defining exponential decay
 def exp_dist(x, A, lamb):
-    B = A*np.exp(-lamb*x)
+    B = A*np.exp(-x/lamb)
     return B
 
 # Generate histogram data for masked data
 data_arr = np.array(data)
-mask = np.ma.masked_where(data_arr > 120) # Mask data which is less than 120MeV
-data_filtered = np.ma.masked_array(data, mask)
-bin_heights_masked, bin_edges_masked = np.histogram(data_filtered, bins=30, range=(104,155))
-bincenters_masked = 0.5*(bin_edges_masked[1:]+bin_edges_masked[:-1])
+data_filtered = np.ma.masked_where(data_arr > 120, data_arr).compressed() # Mask data which is greater than 120MeV
+bin_heights_masked, bin_edges_masked = np.histogram(data_filtered, bins=30, 
+                                                    range=(104,155))
+bin_centers_masked = 0.5*(bin_edges_masked[1:]+bin_edges_masked[:-1])
 mean_std_masked = np.sqrt(bin_heights_masked) # Using Poisson error sqrt for the height
-width_masked = bin_edges_masked[1:]-bin_edges_masked[:-1]
+widths_masked = bin_edges_masked[1:]-bin_edges_masked[:-1]
 
+plt.subplot(2,1,2)
+plt.errorbar(bin_centers_masked, bin_heights_masked, yerr=mean_std_masked, xerr=widths_masked,
+             fmt='.', ecolor='black', elinewidth=0.5, capsize=1)
+plt.ylabel('Number of entries')
+plt.xlabel('Mass (Gev)')
 
+# Determining the value of lambda and A
+# Lambda is 1/mean value
+
+lamb = 1/np.mean(data)
+print(1/lamb)
